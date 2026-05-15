4886827b12f02b70a6ba419723bea9365b775bff
stefannyemilly27 <stefanny.emilly27@gmail.com>
2026-05-14 21:10:26 -0300

    continuando o crud

--- login_acesso/posts/criar.php
+++ login_acesso/posts/criar.php
@@ -2,19 +2,103 @@
 session_start();
 require '../conexao.php';
 
-if (!isset($_SESSION['user'])) {
+if (!isset($_SESSION['email'])) {
 
     header("Location: ../index.php");
     exit;
 }
 
-if (
-    !isset($_SESSION['tipo']) ||
-    $_SESSION['tipo'] != 'admin'
-){
+if ($_SESSION['tipo'] != 'admin'){
     header("location: ../home-comum.php");
     exit;
 }
 
 $erro = "";
 $sucesso = "";
+
+
+$sql = "SELECT * FROM categorias";
+$stmt = $conexao->query($sql);
+$categorias = $stmt->fetchAll();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+$titulo = $_POST['titulo'];
+$conteudo = $_POST['conteudo'];
+$categoria_id = $_POST['categoria_id'];
+$usuario_id = $_SESSION['id'];
+
+if (empty($titulo) || empty($conteudo)) {
+
+    $erro = "Preencha todos os campos!";
+
+} else {
+
+        $sql = "
+        INSERT INTO posts
+        (titulo, conteudo, usuario_id, categoria_id) 
+        VALUES (?, ?, ?, ?)
+        ";
+
+        $stmt = $conexao->prepare($sql);
+
+        $stmt->execute([
+            $titulo,
+            $conteudo,
+            $usuario_id,
+            $categoria_id
+        ]);
+
+        $sucesso = "Post criado com sucesso!!";
+
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Criar Posts</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <div class="box">
+    <h1>Criar Posts</h1>
+
+        <?php if (!empty($erro)) echo "<p class='erro'>$erro</p>"; ?>
+
+        <?php if (!empty($sucesso)) echo "<p class='sucesso'>$sucesso</p>"; ?>
+
+        <form method="POST">
+
+        <input type="text" name="titulo" placeholder="Título do post" required>
+
+        <textarea name="conteudo" placeholder="Conteúdo" required></textarea>
+
+        <select name="categoria_id" required>
+
+        <option value="">Escolha uma categoria</option>
+
+        <?php foreach($categorias as $categoria): ?>
+
+        <option value="<?= $categoria['id'] ?>">
+
+        <?= $categoria['nome'] ?>
+
+        </option>
+
+        <?php endforeach; ?>
+
+        </select>
+
+        <button type="submit">Criar Post</button>
+
+        </form>
+
+    </div>
+</body>
+</html>
---
+++ login_acesso/posts/editar.php
@@ -0,0 +1,134 @@
+<?php
+
+session_start();
+require "../conexao.php";
+
+if (!isset($_SESSION['email'])) {
+
+    header("location: ../index.php");
+    exit;
+}
+
+if ($_SESSION['tipo'] != 'admin'){
+
+    header("location: ../home-comum.php");
+    exit;
+}
+
+$erro = "";
+$sucesso = "";
+
+if (!isset($_GET['id'])){
+    header("location: ../index.php");
+    exit;
+}
+
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM posts WHERE id = ?";
+
+$stmt = $conexao->prepare($sql);
+
+$stmt->execute([$id]);
+
+$post = $stmt->fetch();
+
+if (!$post) {
+    header("location: ../index.php");
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $titulo = $_POST['titulo'];
+    $conteudo = $_POST['conteudo'];
+
+    if (empty($titulo) || empty($conteudo)) {
+
+        $erro = "Preencha os campos!";
+
+    } else {
+
+    $sql = "
+        UPDATE posts
+        SET titulo = ?, conteudo = ?
+        WHERE id = ?
+    ";
+
+
+    $stmt = $conexao->prepare($sql);
+
+    $stmt->execute([
+        $titulo,
+        $conteudo,
+        $id
+    ]);
+
+    $sucesso = "Post atualizado com sucesso!!";
+
+    $post['titulo'] = $titulo;
+    $post['conteudo'] = $conteudo;
+
+
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+
+<meta charset="UTF-8">
+
+<title>Editar Posts</title>
+
+<link rel="stylesheet" href="../../style.css">
+
+</head>
+<body>
+
+<div class="box">
+
+<h1>Editar Post</h1>
+
+
+
+<?php if (!empty($erro)) echo "<p class='erro'>$erro</p>"; ?>
+
+<?php if (!empty($sucesso)) echo "<p class='sucesso'>$sucesso</p>"; ?>
+
+
+
+<form method="POST">
+
+
+
+<input
+type="text"
+name="titulo"
+value="<?= $post['titulo'] ?>"
+required
+>
+
+
+
+<textarea
+name="conteudo"
+required
+><?= $post['conteudo'] ?></textarea>
+
+
+
+<button type="submit">
+
+Salvar Alterações
+
+</button>
+
+</form>
+
+</div>
+
+</body>
+</html>
