baf8e98ea35fdda4dbb357d172dc7d4fdba5ff42
stefannyemilly27 <stefanny.emilly27@gmail.com>
2026-05-20 10:49:00 -0300

    criar.php/categorias

--- login_acesso/categorias/criar.php
+++ login_acesso/categorias/criar.php
@@ -17,4 +17,41 @@ if ($_SESSION['tipo'] != 'admin'){
 $erro = "";
 $sucesso = "";
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $nome = $_POST['nome'];
+
+    if (empty($nome)){
+        $erro = "Preencha o nome da categoria!";
+    } else {
+        $sql = "INSERT INTO categorias (nome)
+        VALUES (?)
+        ";
+
+        $stmt = $conexao->prepare($sql);
+
+        $stmt->execute([$nome]);
+
+        $sucesso = "Categoria criada com sucesso!";
+
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Criar Categorias</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <div class="box">
+    <h1>Criar Categoria</h1>
+    
+    </div>
+</body>
+</html>
+
+
 
